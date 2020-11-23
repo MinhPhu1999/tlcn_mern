65d192d054a161db53e58cd30b3017a9f3e53724
unknown <minhphuson1999@gmail.com>
2020-11-23 21:30:57 +0700

    chinh sua bien moi truong env

--- api/routers/brand.router.js
+++ api/routers/brand.router.js
@@ -1,10 +1,6 @@
 'use strict'
 const brand_controller = require('../controllers/brand.controller');
 module.exports = (app) => {
-    // app.route('/brand')
-    //     .get(brand_controller.getBrand);
-    // app.route('/brand/all/:page')
-    //     .get(brand_controller.getAll);
     app.route('/brand/all/:page')
         .get(brand_controller.getAll);
 
