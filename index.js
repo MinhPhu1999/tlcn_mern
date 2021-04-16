d15193defd3c3286ac7d88866281fcf0cb7ca45d
MinhPhu1999 <minhphuson1999@gmail.com>
2021-04-16 14:05:45 +0700

    socket.io trong index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const connectDB = require('./api/config/connectDB');
+
+
 const userRouter = require('./api/routers/user.router');
 const categoryRouter = require('./api/routers/category.router');
 const productRouter = require('./api/routers/product.router');
@@ -16,28 +18,81 @@ const cartRouter = require('./api/routers/cart.router');
 const orderRouter = require('./api/routers/order.router');
 const adminRouter = require('./api/routers/admin.router');
 
+//model comment
+const Comments = require('./api/models/comment.model');
+
+//kết nối tới database mongo
 connectDB();
 
-// mongoose.Promise = global.Promise;
-// const {mongoURL} = require('./mongo')
-// mongoose.connect(mongoURL,{ //kết nối tới database
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology:true,
-//     useCreateIndex: true}).catch(error => console.log(error.reason));
 
 //có phép nhận dữ liệu từ form
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(cors());
 //cors
-// app.use(function(req,res,next){
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-//     next();
-// });
+app.use(cors());
+
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+// Soketio
+let users = []
+io.on('connection', socket => {
+	
+
+    socket.on('joinRoom', id => {
+        const user = {userId: socket.id, room: id}
+
+        const check = users.every(user => user.userId !== socket.id)
+
+        if(check){
+            users.push(user)
+            socket.join(user.room)
+        }else{
+            users.map(user => {
+                if(user.userId === socket.id){
+                    if(user.room !== id){
+                        socket.leave(user.room)
+                        socket.join(id)
+                        user.room = id
+                    }
+                }
+            })
+        }
+    })
+
+    socket.on('createComment', async msg => {
+        const {username, content, product_id, createdAt, rating, send} = msg
+
+        const newComment = new Comments({
+            username, content, product_id, createdAt, rating
+        })
+
+        if(send === 'replyComment'){
+            const {_id, username, content, product_id, createdAt, rating} = newComment
+
+            const comment = await Comments.findById(product_id)
+
+            if(comment){
+                comment.reply.push({_id, username, content, createdAt, rating})
+
+                await comment.save()
+                io.to(comment.product_id).emit('sendReplyCommentToClient', comment)
+            }
+        }else{
+            await newComment.save()
+            io.to(newComment.product_id).emit('sendCommentToClient', newComment)
+        }
+
+        
+    })
+
+    socket.on('disconnect', () => {
+		
+        users = users.filter(user => user.userId !== socket.id)
+    })
+})
+
 
 userRouter(app);
 categoryRouter(app);
