a82dd5979e261ec7ac47aaced8d7c0a62fe1a73d
MinhPhu1999 <minhphuson1999@gmail.com>
2021-05-18 15:48:23 +0700

    redis env

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -11,7 +11,10 @@ const ord = require('../utils/orderby');
 require('dotenv').config();
 const { performance } = require('perf_hooks');
 const redis = require('redis');
-const client = redis.createClient(process.env.REDIS_ENDPOINT_URI);
+const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOSTNAME, {
+    no_ready_check: true,
+    auth_pass: process.env.REDIS_PASSWORD,
+});
 
 exports.addOrder = async (req, res) => {
     //kiểm tra có truyền tham số đủ hay không
