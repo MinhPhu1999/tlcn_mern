3618e794260d4e1ef1eeec147f493a1d2d9ba693
MinhPhu1999 <minhphuson1999@gmail.com>
2021-06-29 17:24:12 +0700

    subtotal

--- api/routers/admin.router.js
+++ api/routers/admin.router.js
@@ -61,7 +61,8 @@ module.exports = app => {
     app.route('/admin/order/quantitybyyear/:year').get(order_controller.getQuantityByYear);
 
 	app.route('/admin/order/subtotal/:year').get(order_controller.getOrderSubTotalByYear);
-	app.route('/admin/order/subtotalcategory/:year').post(order_controller.getOrderSubTotalByYearAndCategory);
+	
+	app.route('/admin/order/subtotalcategory').post(order_controller.getOrderSubTotalByYearAndCategory);
 
     app.route('/admin/order/yearandcategory').post(order_controller.getQuantityByYearAndCategory);
 
